50e1d1753eaeb387382141bbdb44ef67865732e8
Asocia <akkayas17@itu.edu.tr>
2019-05-08 23:55:21 +0300

    OOP-HW3 Add new helper functions

--- 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/helper_functions.cpp
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/helper_functions.cpp
@@ -1,12 +1,13 @@
 #include "helper_functions.h"
+#include <algorithm>
 void split(string line, string delim, vector<string>& fields, int num_of_fields){
     int first = 0, last = 0;
     //cout << line << endl;
-    for(int i = 0; i<num_of_fields;i++){
+    for(int i = 0; i< num_of_fields || num_of_fields == -1;i++){
         last = line.find(delim, first);
         fields.push_back(line.substr(first, last-first));
         //cout << line.substr(first, last-first) << endl;
-        cout << "first:" << first << " last: "<< last << endl;
+        // cout << "first:" << first << " last: "<< last << endl;
         if(first>last){
             break;
         }
@@ -14,4 +15,20 @@ void split(string line, string delim, vector<string>& fields, int num_of_fields)
         
         
     }
+}
+
+bool vector_contains(const vector<string>& v, string& t)
+{
+    bool found = (std::find(v.begin(), v.end(), t) != v.end());
+    return found;
+}
+
+string join(string join_with, vector<string>& v, int begin, int end){
+    string s = "";
+    if(end == -1)
+        end = (int) v.size();
+    for(int i = begin; i< end; i++){
+        s += join_with + v[i];
+    }
+    return s.substr(1,s.length()-1);
 }
--- 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/helper_functions.h
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/helper_functions.h
@@ -4,3 +4,7 @@ using namespace std;
 
 
 void split(string line, string delim, vector<string>& fields, int num_of_fields);
+
+bool vector_contains(const vector<string>& v, string& t);
+
+string join(string join_with, vector<string>& v, int begin = 0, int end = -1);
