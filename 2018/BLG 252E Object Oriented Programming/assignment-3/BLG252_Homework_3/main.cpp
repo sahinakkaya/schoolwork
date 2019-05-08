6cae4581f7672bb02a09de805075c9dd4b54d318
Asocia <akkayas17@itu.edu.tr>
2019-05-09 01:28:20 +0300

    OOP-HW3 Add main file

--- 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/main.cpp
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/main.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <map>
-
+#include "stock.h"
+#include "menu.h"
+#include "order.h"
 using namespace std;
 
-bool open_files(const string file_names[], int length, map <string, ifstream*>& files);
-void close_files(const string file_names[], int length, map <string, ifstream*>& files);
 
 int main(){
     string file_names[] = {"stock", "menu", "order"};
@@ -13,8 +10,13 @@ int main(){
     if(!open_files(file_names, 3, files)) // If cannot open any of the files
         return EXIT_FAILURE;
     
-    
-
+    Stock s(*files["stock"]);
+    //s.write_to_file();
+    //s.print();
+    Menu m(*files["menu"]);
+    //m.print();
+    Order o(*files["order"]);
+    o.print();
 
 
     close_files(file_names, 3, files);
@@ -23,22 +25,3 @@ int main(){
 }
 
 
-
-bool open_files(const string file_names[], int length, map <string, ifstream*>& files){
-    for(int i = 0; i<length;i++){
-        files[file_names[i]] = new ifstream(file_names[i] + ".txt");
-        if(files[file_names[i]]->fail()){
-            close_files(file_names, i+1, files);
-            cerr << "Cannot open file '" << file_names[i] << ".txt'. No such file." << endl;
-            return false;
-        }
-    }
-    return true;
-}
-
-void close_files(const string file_names[], int length, map <string, ifstream*>& files){
-    for(int i = 0; i<length;i++){
-        files[file_names[i]]->close();
-        delete files[file_names[i]];
-    }
-}
