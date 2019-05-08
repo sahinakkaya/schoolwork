c4838278e579afb7f4a24e74ad1007dee8b15768
Asocia <akkayas17@itu.edu.tr>
2019-05-08 23:04:10 +0300

    OOP-HW3 Implement Ingredient class

--- 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/ingredient.cpp
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/ingredient.cpp
@@ -1,6 +1,19 @@
 #include "ingredient.h"
-using namespace std;
-Ingredient::Ingredient(string name, char type, int item_count, double price): 
-    name(name), type(type), item_count(item_count), price(price)  { 
 
+using namespace std;
+
+BaseIngredient::BaseIngredient(string name, int item_count, double price){
+    this->name = name;
+    this->item_count = item_count;
+    this->price = price;
 }
+
+map<char, string> Ingredient::unit_of_types = {{'1', "gram"}, {'2',""}, {'3',"ml"}};
+Ingredient::Ingredient(string name, string type, string item_count, string price): 
+            BaseIngredient(name, stoi(item_count), stod(price))
+{ 
+    this->type = type[0];
+    unit = unit_of_types[this->type];
+}
+
+
--- 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/ingredient.h
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/ingredient.h
@@ -1,13 +1,33 @@
+#ifndef INGREDIENT_H
+#define INGREDIENT_H
 #include <iostream>
+#include "helper_functions.h"
+#include <map>
 using namespace std;
-class Ingredient{
+
+class BaseIngredient{
     protected:
         string name;
-        char type;
         int item_count;
         double price;
     public:
-        Ingredient(string, char, int, double);
-        double get_cost(int amount) {return amount*price;}
+        BaseIngredient(string, int, double);
+        virtual double get_cost(int) const = 0;
+        string get_name() const {return name;}
+        int get_item_count() const{return item_count;}
+        void set_item_count(int ic) {item_count = ic;} 
+        double get_price() const {return price;}
+};
+class Ingredient: public BaseIngredient{
+    private:
+        char type;
+        string unit;
+    public:
+        static map<char, string> unit_of_types;
+        Ingredient(string name, string type, string item_count, string price);
+        double get_cost(int amount) const {return amount * price;}
+        char get_type() const {return type;}
         
 };
+
+#endif
