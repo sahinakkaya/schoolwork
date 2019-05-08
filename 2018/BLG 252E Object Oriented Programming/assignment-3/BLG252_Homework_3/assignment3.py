29437bb4d4828aa189c3d4917e90f592a3d40b36
Asocia <akkayas17@itu.edu.tr>
2019-05-08 04:39:43 +0300

    Implement homework 3 of OOP course in python

--- 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/assignment3.py
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/assignment3.py
@@ -1,5 +1,4 @@
 import sys
-import re
 
 
 class BaseIngredientClass:
@@ -8,7 +7,7 @@ class BaseIngredientClass:
         self.item_count = int(item_count)
         self.price = float(price)
 
-    def calculate_cost(self, quantity):
+    def get_cost(self, quantity):
         return self.price * quantity
 
 
@@ -28,7 +27,7 @@ class Stock:
     def __init__(self, file_name):
         self.file_name = file_name
         self.header = None
-        self.stock = []
+        self.stock = {}
         self.init_stock()
 
     def init_stock(self):
@@ -36,12 +35,21 @@ class Stock:
             self.header = f.readline().strip()
             for line in f.readlines():
                 name, type_, item_count, price = line.strip().split("\t")
-                self.stock.append(Ingredient(name, type_, item_count, price))
+                self.stock[name] = Ingredient(name, type_, item_count, price)
 
     def print(self, file=sys.stdout):
         print(self.header, file=file)
-        for ingredient in self.stock:
+        for ingredient in self.stock.values():
             print(*ingredient, sep="\t", file=file)
+
+    def has_enough_ingredient(self, name, amount):
+        return self.stock[name].item_count >= amount
+
+    def update(self, name, amount):
+        self.stock[name].item_count -= amount
+
+    def get_price(self, name, amount):
+        return self.stock[name].get_cost(amount)
 
     def update_stock(self):
         f = open(self.file_name, "w")
@@ -53,7 +61,7 @@ class Menu:
     def __init__(self, file_name):
         self.file_name = file_name
         self.header = None
-        self.menu = []
+        self.menu = {}
         self.init_menu()
 
     def init_menu(self):
@@ -64,28 +72,31 @@ class Menu:
                 name, ingredients = line.strip().split("\t")
 
                 if ingredients == "N/A":
-                    self.menu.append((name, []))
+                    self.menu[name] = (name, 1),
                 else:
-                    l = []
+                    ingredients_list = []
                     for ingredient in ingredients.split(", "):
                         required_amount = int(ingredient.split()[0])
                         if ingredient.split()[1] in units:
                             ingredient_name = " ".join(ingredient.split()[2:])
                         else:
                             ingredient_name = " ".join(ingredient.split()[1:])
-                        l.append((ingredient_name, required_amount))
-                    self.menu.append((name, l))
+                        ingredients_list.append((ingredient_name, required_amount))
+                    self.menu[name] = ingredients_list
 
     def print(self):
         for item in self.menu:
-            print(item)
+            print(item, self.menu[item])
 
 
 class Order:
-    def __init__(self, file_name):
+    def __init__(self, file_name, menu, stock):
         self.file_name = file_name
+        self.stock = stock
+        self.menu = menu
         self.tables = []
         self.init_tables()
+        self.serve_to_tables()
 
     def init_tables(self):
         f = open(self.file_name)
@@ -99,7 +110,7 @@ class Order:
                 amount, name = f.readline().strip().split(" ", 1)
                 amount = int(amount)
                 orders.append((name, amount))
-            self.tables.append((table_name, orders))
+            self.tables.append((table_name, *orders))
 
         f.close()
 
@@ -107,10 +118,43 @@ class Order:
         for table in self.tables:
             print(table)
 
+    def serve_to_tables(self):
+        for table in self.tables:
+            table_name, *orders = table
+            print(f"{table_name} ordered:")
+            total_cost = 0
+            for order in orders:
+                order_name, amount = order
+                requirements = self.menu.menu[order_name]
+                can_be_served = all(self.stock.has_enough_ingredient(i[0], i[1] * amount) for i in requirements)
+                if not can_be_served:
+                    print(f"We don't have enough {order_name}")
+                while not all(
+                        self.stock.has_enough_ingredient(i[0], i[1] * amount) for i in requirements) and amount > 0:
+                    amount -= 1
+                if amount > 0:
+                    cost = 0
+                    for req in requirements:
+                        self.stock.update(req[0], req[1] * amount)
+                        cost += self.stock.get_price(req[0], req[1])
+                    cost *= amount
+                    print(f"{amount} {order_name} cost: {int(cost)}")
+                    total_cost += cost
+            tip = total_cost * 0.15
+            tax = total_cost * 0.08
+            total_cost = total_cost + tip + tax
+            print(f"Tip is {tip:.2g}")
+            print(total_cost)
+            print(f"Total cost: {total_cost:.2g} TL")
+            print("*" * 25)
+
+
 if __name__ == '__main__':
     s = Stock("stock.txt")
     # s.print()
     m = Menu("menu.txt")
     # m.print()
-    t = Order("order.txt")
+    t = Order("order.txt", m, s)
     # t.print()
+
+    s.update_stock()
