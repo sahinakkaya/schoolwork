0394f4a8d82e09beb638beac1ca23995bae7f32c
Asocia <akkayas17@itu.edu.tr>
2019-05-08 23:06:09 +0300

    OOP-HW3 Implement Stock class

---
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/stock.cpp
@@ -0,0 +1,54 @@
+#include "stock.h"
+#include "vector"
+using namespace std;
+
+
+Stock::Stock(ifstream& file){
+    getline(file, header);
+    //cout << header << endl;
+    string line;
+    while(getline(file, line)){
+        vector<string> fields;
+        split(line, "\t", fields, 4);
+        Ingredient* ing = new Ingredient(fields[0],fields[1],fields[2],fields[3]);
+        stock[fields[0]] = ing;
+    }
+    
+};
+
+
+
+bool Stock::has_enough_ingredient(string name, int amount){
+    return stock[name]->get_item_count() >= amount;
+}
+
+void Stock::update(string name, int amount){
+    // self.stock[name].item_count -= amount
+    int new_item_count = stock[name]->get_item_count() - amount;
+    stock[name]->set_item_count(new_item_count);
+}
+
+double Stock::get_price(string name, int amount){
+    // return self.stock[name].get_cost(amount)
+    return stock[name]->get_cost(amount);
+}
+
+void Stock::print(FILE* file = stdout){
+    FILE fp_old = *stdout;  // preserve the original stdout
+    *stdout = *file;
+    cout << header <<endl;
+    for(auto const& x : stock){
+        Ingredient* ing = x.second;
+        cout << ing->get_name() << '\t' 
+             << ing->get_type() << '\t' 
+             << ing->get_item_count() << '\t'
+             << ing->get_price() << endl;
+    }
+    *stdout=fp_old;
+}
+
+void Stock::write_to_file(const char* file_name){
+    FILE* f = fopen(file_name, "w");
+    print(f);
+    fclose(f);
+}
--- 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/stock.h
+++ 2018/BLG 252E Object Oriented Programming/assignment-3/BLG252_Homework_3/stock.h
@@ -1,6 +1,17 @@
 #include "ingredient.h"
-#define NUM_OF_INGREDIENTS 27
-class Stock{
-    Ingredient* ingredients[NUM_OF_INGREDIENTS];
+#include <fstream>
+#include <vector>
+using namespace std;
 
-}
+class Stock{
+    string header;
+    map<string, Ingredient*> stock;
+/*     void split(string, char, vector<string>&); */
+    public:
+        Stock(ifstream&);
+        bool has_enough_ingredient(string, int);
+        void update(string, int);
+        double get_price(string, int);
+        void print(FILE*);
+        void write_to_file(const char* filename="stock.txt");
+};
