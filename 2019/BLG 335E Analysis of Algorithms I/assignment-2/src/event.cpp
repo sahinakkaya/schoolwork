ef1d6d8b92da0e873df97bcaf61686f55bdf25c2
Asocia <akkayas17@itu.edu.tr>
2019-12-11 19:04:44 +0300

    Create header and source files for the event

---
+++ 2019/BLG 335E Analysis of Algorithms I/assignment-2/src/event.cpp
@@ -0,0 +1,7 @@
+/* @Author
+* Student Name: Şahin Akkaya
+* Student ID : 150170098
+* Date: 11.12.2019 */
+
+
+#include "event.h"
---
+++ 2019/BLG 335E Analysis of Algorithms I/assignment-2/src/event.h
@@ -0,0 +1,10 @@
+/* @Author
+* Student Name: Şahin Akkaya
+* Student ID : 150170098
+* Date: 11.12.2019 */
+
+#ifndef EVENT_H
+#define EVENT_H
+
+
+#endif
