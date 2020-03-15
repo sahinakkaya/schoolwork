6ff5856077986d72c7f01e28fd31b1490f8fbc03
Asocia <akkayas17@itu.edu.tr>
2020-03-16 01:32:25 +0300

    Print output for first 2 level

--- 2019/BLG 312E Computer Operating Systems/assignment-1/src/assignment1.c
+++ 2019/BLG 312E Computer Operating Systems/assignment-1/src/assignment1.c
@@ -1,122 +1,118 @@
 #include <stdio.h>
-#include <sys/wait.h>  //   wait
-#include <unistd.h>    //fork
+#include <sys/wait.h>  // wait
+#include <unistd.h>    // fork
 
-/*int main(int argc, char* argv[]) {
-  int c = 0;
-  int child = fork();
-  c++;
-
-  if (child == 0) {
-    child = fork();
-    c += 2;
-    if (child) {
-      c = c * 3;
-      sleep(3);
-      printf("pid: %d c: %d \n", getpid(), c);
-    } else {
-      sleep(2);
-      printf("pid: %d c: %d \n", getpid(), c);
-
+void print_info_about_process(int child_pids[], int num_of_childs, int level) {
+  printf("<%d, ", getpid());
+  if (num_of_childs) {
+    printf("[");
+    for (int i = 0; i < num_of_childs; i++) {
+      if (i != 0) {
+        printf(", ");
+      }
+      printf("%d", child_pids[i]);
     }
-
+    printf("]");
   } else {
-    c += 4;
-    sleep(1);
+    printf("None");
+  }
+  printf(", %d>\n", level);
+}
+
+int main() {
+  int level1_childs_pid[3] = {0};
+  int child = fork();
+  if (child == 0) {
+    // 2
+    int level2_childs_pid[2] = {0};
     child = fork();
     if (child == 0) {
-      sleep(4);
+      // 3
+      child = fork();
+      if (child == 0) {
+        // 4
+      } else {
+        // 3
+      }
+      // 3,4
+    } else {
+      level2_childs_pid[0] = child;
+      // 2
+      child = fork();
+      if (child == 0) {
+        // 5
+      } else {
+        level2_childs_pid[1] = child;
+        print_info_about_process(level2_childs_pid, 2, 2);
+      }
+      // 2, 5
     }
-    printf("pid: %d c: %d \n", getpid(), c);
+    // 2, 3, 4, 5
+  } else {
+    level1_childs_pid[0] = child;
+    child = fork();
+    if (child == 0) {
+      // 6
+      int level2_childs_pid[2] = {0};
+      child = fork();
+      if (child == 0) {
+        // 7
+        child = fork();
+        if (child == 0) {
+          // 8
+        } else {
+          // 7
+        }
+        // 7
+      } else {
+        // 6
+        level2_childs_pid[0] = child;
+        child = fork();
+        if (child == 0) {
+          // 9
+        } else {
+          // 6
+          level2_childs_pid[1] = child;
+          print_info_about_process(level2_childs_pid, 2, 2);
+        }
+        // 6
+      }
+
+    } else {
+      // 1
+      level1_childs_pid[1] = child;
+      child = fork();
+      if (child == 0) {
+        // 10
+        int level2_childs_pid[2] = {0};
+        child = fork();
+        if (child == 0) {
+          // 11
+          child = fork();
+          if (child == 0) {
+            // 12
+          }
+          // 11, 12
+        } else {
+          // 10
+          level2_childs_pid[0] = child;
+          child = fork();
+          if (child == 0) {
+            // 13
+          } else {
+            level2_childs_pid[1] = child;
+            print_info_about_process(level2_childs_pid, 2, 2);
+          }
+          // 10 ,13
+        }
+        // 10, 11, 12, 13
+      } else {
+        level1_childs_pid[2] = child;
+        print_info_about_process(level1_childs_pid, 3, 1);
+      }
+      // 1, 10, 11, 12, 13
+    }
   }
-  sleep(5);
+
   return 0;
-}*/
-
-
-int main(){
-    int child = fork();
-    if (child == 0){
-        // 2
-        child = fork();
-        if (child == 0){
-            // 3
-            child = fork();
-            if (child == 0){
-                //4
-            }
-            else{
-                // 3
-            }
-            // 3,4
-        } 
-        else{
-            // 2
-            child = fork();
-            if (child == 0){
-                // 5
-            }
-            // 2, 5
-        }
-        // 2, 3, 4, 5
-    }else{
-        child = fork();
-        if (child == 0){
-            // 6
-            child = fork();
-            if (child == 0){
-                // 7
-                child = fork();
-                if (child == 0){
-                    // 8
-                }
-                else{
-                    // 7
-                }
-                // 7
-            }
-            else{
-                // 6
-                child = fork();
-                if (child == 0){
-                    // 9
-                }
-                else{
-                    // 6
-                }
-                // 6
-            }
-
-
-        } else{
-            // 1
-            child = fork();
-            if (child == 0){
-                // 10
-                child = fork();
-                if (child == 0){
-                    // 11
-                    child = fork();
-                    if (child == 0){
-                        // 12
-                    }
-                    // 11, 12
-                }
-                else {
-                    // 10
-                    child = fork();
-                    if (child == 0){
-                        // 13
-                    }
-                    // 10 ,13
-                }
-                // 10, 11, 12, 13
-            }
-            // 1, 10, 11, 12, 13
-        }
-        
-    }
-
-    return 0;
-
 }
