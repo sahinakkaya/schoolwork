867e0891ccc5638925050bfaec942a71b9c0f985
Asocia <akkayas17@itu.edu.tr>
2020-03-24 19:50:54 +0300

    Complete first part of the assignment

--- 2019/BLG 336E Analysis of Algorithms II/assignment-1/src/assignment1.cpp
+++ 2019/BLG 336E Analysis of Algorithms II/assignment-1/src/assignment1.cpp
@@ -1,22 +1,26 @@
-#include "attack.h"
-#include "pokemon.h"
-#include "node.h"
+#include "tree.h"
 using namespace std;
-int main(){
+int main(int argc, char**argv){
     Attack a = Attack("Thundershock","-10","100","40","0");
     //cout << a.cost << endl;
     //cout << Pokemon::ATTACKS["blastoise"][1].name << endl;
 
     Pokemon pikachu = Pokemon("pikachu", 200, 100);
     Pokemon blastoise = Pokemon("blastoise", 200, 100);
-    pikachu.attack(blastoise, a, false);
-    Node n = Node(make_pair(pikachu, blastoise), 0.5, 0, nullptr);
-    cout << (n.parent == nullptr) << endl;
-    Node p = Node(make_pair(pikachu, blastoise), 0.5, 0, &n);
-    cout << (p.parent->parent == nullptr) << endl;
+    //pikachu.attack(blastoise, a, false);
+    Node n = Node(make_pair(&pikachu, &blastoise), 1, 0, nullptr);
+    // cout << (n.parent == nullptr) << endl;
+    // Node p = Node(make_pair(pikachu, blastoise), 0.5, 0, &n);
+    // cout << (p.parent->parent == nullptr) << endl;
 
-
-
+    Tree tree;
+    tree.create_root(&n);
+    int max_level;
+    string part = argv[1];
+    if (part == "part1"){
+        max_level = stoi(argv[2]);
+        tree.create_levels_until(max_level);
+    }
     //Pokemon p = pikachu.copy();
     //p.health_points = 0;
     //cout << p.health_points<< endl;
