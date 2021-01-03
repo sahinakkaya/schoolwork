28a23ae5fcf32a067a3f07532c86e7f4035ff44f
Asocia <akkayas17@itu.edu.tr>
2021-01-03 11:38:11 +0300

    Implement successor function

--- 2020/BLG 435E Artificial Intelligence/assignment-2/src/minimax.py
+++ 2020/BLG 435E Artificial Intelligence/assignment-2/src/minimax.py
@@ -1,3 +1,4 @@
+import numpy as np
 def chunks(l, n):
     """Yield successive n-sized chunks from lst."""
     n = max(1, n)
@@ -31,8 +32,55 @@ class Game:
         with open(file_name) as f:
             grid = [list(map(int, line.strip().split())) for line in f]
 
+        grid = np.array(grid)
         turn = 'MAX'
         return State(grid, turn)
+
+    def successors(self, state):
+        """
+        Returns a dictionary of (coordinate, available_numbers) pairs
+        """
+        empty_positions = np.argwhere(game.initial_state.grid == 0)
+        return {(i, j): self._get_available_numbers(state.grid, i, j)
+                for i, j in empty_positions}
+
+    @staticmethod
+    def _get_box_numbers(grid, i, j):
+        """
+        Returns available numbers for given position considering only the 3x3
+        box around the position
+        """
+        top, left = i - i % 3, j - j % 3
+        available_numbers = set(range(1, 10))
+        for i in range(3):
+            for j in range(3):
+                current_num = grid[top+i][left+j]
+                if current_num != 0:
+                    available_numbers.remove(current_num)
+        return available_numbers
+
+    @staticmethod
+    def _get_row_numbers(grid, row_index):
+        """
+        Returns available numbers for given position considering only the row
+        of the position
+        """
+        return set(range(1, 10)).difference(grid[row_index])
+
+    @staticmethod
+    def _get_col_numbers(grid, col_index):
+        """
+        Returns available numbers for given position considering only the
+        column of the position
+        """
+        return set(range(1, 10)).difference(grid[:, col_index])
+
+    def _get_available_numbers(self, grid, row_index, col_index):
+        """ Returns available numbers for given position """
+        box = self._get_box_numbers(grid, row_index, col_index)
+        row = self._get_row_numbers(grid, row_index)
+        col = self._get_col_numbers(grid, col_index)
+        return box.intersection(row).intersection(col)
 
 
 if __name__ == '__main__':
